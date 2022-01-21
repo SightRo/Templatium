7b9264492593b5f83b5da59cd1aadff4255546a5
SightRo <tagir00123@gmail.com>
2022-01-21 07:55:25 +0300

    Add default processors list

---
+++ Templatium.Docx/Processors/DefaultProcessors.fs
@@ -0,0 +1,12 @@
+﻿namespace Templatium.Docx.Processors
+
+open Templatium.Docx
+open Templatium.Docx.Processors
+
+[<RequireQualifiedAccess>]
+module Processors =
+    let defaults: IProcessor seq =
+        [ StringProcessor()
+          ImageProcessor()
+          TableProcessor()
+          CheckBoxProcessor() ]
