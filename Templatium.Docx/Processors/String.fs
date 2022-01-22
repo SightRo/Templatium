7b287732b4bc2a2013e10045051348f8b741e2aa
SightRo <tagir00123@gmail.com>
2022-01-22 21:41:48 +0300

    Fix string processor

--- Templatium.Docx/Processors/String.fs
+++ Templatium.Docx/Processors/String.fs
@@ -16,16 +16,25 @@ type StringProcessor() =
 
             match contentNode with
             | Some block ->
-                let textNode =
-                    block.Descendants<Text>().FirstOrDefault()
+                let textNodes = block.Descendants<Text>().ToList()
 
-                match textNode with
-                | null ->
+                match textNodes.Count with
+                | 0 ->
+                    // This block will never executes if document created using Word
+                    // At least the latest Word insert placeholder text in any case
+                    // So it not tested in any sense
                     block.RemoveAllChildren()
 
                     block.AppendChild(Paragraph(Run(Text(stringContent.Value))))
                     |> ignore
-                | _ -> textNode.Text <- stringContent.Value
+                | _ ->
+                    // If text is multiline, Word insert multiple text nodes (one per line)
+                    // So we modify the first one to preserve formatting and delete the rest
+                    textNodes[0].Text <- stringContent.Value
+
+                    textNodes
+                    |> Seq.skip 1
+                    |> Seq.iter (fun n -> n.Remove())
             | _ -> ()
 
             ()
