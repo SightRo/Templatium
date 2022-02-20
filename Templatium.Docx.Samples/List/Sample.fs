ff3dcc6ba16f87e1e24e78481fdac072d8281e09
SightRo <tagir00123@gmail.com>
2022-02-20 20:44:47 +0300

    Add list processor

---
+++ Templatium.Docx.Samples/List/Sample.fs
@@ -0,0 +1,35 @@
+﻿module internal Templatium.Docx.Samples.List
+
+open System.IO
+open DocumentFormat.OpenXml.Packaging
+open Templatium.Docx
+open Templatium.Docx.Processors
+
+[<Literal>]
+let inputPath = __SOURCE_DIRECTORY__ + "/input.docx"
+
+[<Literal>]
+let outputPath = __SOURCE_DIRECTORY__ + "/output.docx"
+
+let run () =
+   
+    let list: List<IContent> = [
+        { Title = "Name"; Value = "Bread" }
+        { Title = "Name"; Value = "Milk" }
+        { Title = "Name"; Value = "Spaghetti" }
+    ]
+
+    let listContent =
+        { Title = "List"
+          Items = list }
+
+    use ms = new MemoryStream()
+    ms.Write(File.ReadAllBytes(inputPath))
+    use doc = WordprocessingDocument.Open(ms, true)
+
+    doc
+    |> DocxTemplater.fillDocument Processors.defaults [ listContent ]
+    |> DocxTemplater.deleteContentControls
+
+    doc.SaveAs outputPath |> ignore
+    ()
